d7c86830c3f884fd548a0ddb6001569dc6dde738
HASSAN A <87488089+branwith@users.noreply.github.com>
2022-11-07 18:15:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,15 @@ import { render } from "preact";
 import { App } from "./app";
 
 render(<App />, document.getElementById("app"));
+import { Analytics } from '@vercel/analytics/react';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+
+export default MyApp;
